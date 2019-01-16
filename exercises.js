e1c0670e28652bde4e7eea4b5dc1cddf6dee0b46
kmietotwy <kmietotwy@gmail.com>
2019-01-15 19:22:08 -1000

    completed Line Number exercise

--- exercises.js
+++ exercises.js
@@ -44,6 +44,7 @@ The value at 4 is Monroe.
 
 Next, console.log the length of the array.
 
+
 3-A) Create a function named leaders which will take in a parameter: `person`.
    @param Datatype: String `person`
  
@@ -52,12 +53,36 @@ This function will iterate through the person parameter and console.log the foll
 "President person was a great leader."
 */
 
+var presidents = ['Washington', 'Adams', 'Jefferson', 'Madison', 'Monroe'];
+
+for (var i=0; i<presidents.length; i++){
+   console.log('The value at ' + i + ' is ' + presidents[i] + '.');
+}
+
+console.log('presidents length:', presidents.length);
+
+function leaders(person){
+   for (var i=0; i<presidents.length; i++){
+      console.log('President ' + presidents[i] + ' was a great leader.');
+   }
+
+}
+
+leaders(presidents);
+
+
 
 /* 4) Line Number
 Declare a variable named `stringOfNumbers` and assign its value to an empty string. 
 
 Write a for-loop that concatenates a Number value into that string on each iteration, starting at `10` and continuing up to and including `20`. Console.log your result. It should read "1011121314151617181920"*/
 
+var stringOfNumbers = '';
+
+for (var i=10; i<21; i++){
+   stringOfNumbers += i;
+}
+console.log(stringOfNumbers);
 
 /* 5) Even Stevens
 Declare a variable named `evenNumArr` and assign its value to an empty array. 
